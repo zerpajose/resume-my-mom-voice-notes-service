99f694360080ec460790305c2c39facf080859e2
zerpajose <zjose88@gmail.com>
2025-07-26 21:27:05 -0400

    fix: revert port configuration to default value in server setup

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 import app from './app.js';
-import config from './src/config/environment.js';
 
 (async () => {
-  const port = process.env.PORT ? Number(process.env.PORT) : config.port;
+  const port = process.env.PORT ? Number(process.env.PORT) : 8080;
   app.listen(port, () => {
     console.log(`Server started on port ${port}`);
   });
